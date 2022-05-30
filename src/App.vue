8516a585ed0eb3b15342315f8eea20b6753f38d2
Hiempsal-malamute <51490676+Hiempsal-malamute@users.noreply.github.com>
2022-05-30 21:33:25 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Portfolio</router-link>
+    <router-link to="/about">À propos</router-link>
+  </div>
+  <router-view/>
+</template>
+
+<style>
+  @font-face {
+    font-family: 'Coda-Regular';
+    src: url('./assets/fonts/Coda-Regular.ttf');
+  }
+  
+  @font-face {
+    font-family: 'Coda-ExtraBold';
+    src: url('./assets/fonts/Coda-ExtraBold.ttf');
+  }
+
+  #app {
+    font-family: 'Coda-Regular', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+  #nav {
+    display: flex;
+    padding: 30px;
+    justify-content: flex-end;
+  }
+
+  #nav a {
+    font-weight: bold;
+    text-transform: uppercase;
+    color: #2c3e50;
+    margin: 5px 10px 2px 10px;
+    text-decoration: none;
+    display: inline-block;
+  }
+
+  #nav a.router-link-exact-active {
+    color: #42b983;
+  }
+
+    /*Hover effect underline*/
+  #nav a::after {
+    content: "";
+    display:block;
+    margin:0 auto;
+    margin-bottom: -2px;
+    height: 2px;
+    background-color: #42b983;
+    visibility: hidden;
+    z-index: 1000;
+    transform: scaleX(0);
+    transform-origin:left;
+    transition: transform 250ms ease-in-out;
+  }
+
+  #nav a:hover:after {
+    visibility: visible;
+    transform: scaleX(1);
+    transform-origin:left;
+  }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h1>Hassen Chougar</h1>
+    <span>Cartographe-géomaticien</span>
+    <p>
+      Un site web minimaliste dont l'ambition est d'exposer un modeste portfolio cartographique.
+    </p>
+    <h3>Contact</h3>
+    <ul>
+      <li><a href="mailto:https://chat.vuejs.org">Email</a></li>
+      <li><a href="https://www.linkedin.com/in/hassen-chougar-53a0aab5/" target="_blank" rel="noopener">Linkedin</a></li>
+      <li><a href="https://github.com/Hiempsal-malamute/" target="_blank" rel="noopener">Github</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
